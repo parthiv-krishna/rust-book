c00657868c9b4abba3928174875446b7de5f9f06
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2023-07-09 23:49:20 -0500

    Chapter 6.3: Concise Control Flow with if let

---
+++ chapter_06/if_let/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    let config_max = Some(3u8);
+    match config_max {
+        Some(max) => println!("The maximum is configured to be {}", max),
+        _ => (),
+    }
+
+    // more concise, avoids the catch all at the end
+    let config_max = Some(3u8);
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {}", max);
+    }
+
+
+    /*
+    let mut count = 0;
+    match coin {
+        Coin::Quarter(state) => println!("State quarter from {:?}!", state),
+        _ => count += 1,
+    }
+
+    can become
+    
+    let mut count = 0;
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else { // not a Coin::Quarter(state)
+        count += 1;
+    }
+     */
+
+}
